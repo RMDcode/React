dce404041437e839227ec5be447145d06180c4dd
Rohit Dhuri <128274063+RMDcode@users.noreply.github.com>
2024-04-02 10:08:49 +0530

    Add files via upload

---
+++ src_Challenge1/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <>
+    <h4>First Chanllenge for MCU movies in Order</h4>
+    <h3>Top 5 MCU movies.</h3>
+    <ol>
+    <li>Infinity War </li>
+    <li>End Game</li>
+    <li>Civil War</li>
+    <li>Spiderman: No way home</li>
+    <li>Deadpool and wolverine</li>
+    </ol>
+  </>    
+  ,document.getElementById('root')
+);
