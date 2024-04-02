b9f4e2e6a0ccb2d44e5658ac770dc201f806b010
Rohit Dhuri <128274063+RMDcode@users.noreply.github.com>
2024-04-02 10:16:32 +0530

    Add files via upload

---
+++ src-WithComponents/Paragraph.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Paragraph(){
+    return(<>
+    <h3>Hi there,</h3>
+    <p>Have a Nice Day!</p>
+    <p>Please find attached my resume for your review.</p>
+    <p>Please let me know if you have any vacany related to Front End Developer.</p>
+    </>);
+}
+
+export default Paragraph;
---
+++ src-WithComponents/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Heading from './Heading';
+import Paragraph from './Paragraph';
+import Topic from './Topic';
+import Regards from './Regards';
+
+ReactDOM.render(
+  <>
+    <Heading/>
+    <Topic />
+    <Paragraph/>
+    <Regards />
+  </>
+  ,document.getElementById("root")
+);
