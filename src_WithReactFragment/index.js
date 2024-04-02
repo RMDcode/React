56ea6ca5fe2924cf9991858494fc867ed6d64dc0
Rohit Dhuri <128274063+RMDcode@users.noreply.github.com>
2024-04-02 10:24:02 +0530

    Add files via upload

---
+++ src_WithReactFragment/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//JSX-code
+
+ReactDOM.render(
+  <>
+    <h1>Thapa Technical</h1>
+    <p>Plz like and share to all</p>
+    <h2>Plz Subscribe my channel</h2>
+  </>,
+  document.getElementById("root")
+);
